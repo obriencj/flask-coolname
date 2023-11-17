dd947e1f7756ef034cd0d27399f46d86089545c7
Christopher O'Brien <obriencj@gmail.com>
2023-11-17 13:09:06 -0500

    setting some defaults

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,8 +1,18 @@
 # Put gunicorn setting overrides here
 #
 
-
 bind = "0.0.0.0:8080"
+
+# puts the coolname words into memory for workers before fork
+preload_app = True
+
+# afaict this is used for fast sending of the static content
+sendfile = True
+
+# set to (cores * 2) + 1
+workers = 5
+
+worker_class = "sync"
 
 
 # The end.
