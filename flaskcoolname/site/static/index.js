1b3bd549a08ded6ac887c8d1ae094a9a446b27d1
Christopher O'Brien <obriencj@gmail.com>
2023-11-26 08:48:20 -0500

    license and flow control fix

--- flaskcoolname/site/static/index.js
+++ flaskcoolname/site/static/index.js
@@ -1,5 +1,21 @@
 /*
-  Ms. Sluggy Slugwoth Slugworm (huaghhh)
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Flask Coolname - A simple web UI and RESTful interfae to coolname
+
+  author: Christopher O'Brien <obriencj@gmail.com>
+  license: AGPL v3
 */
 
 
@@ -143,6 +159,8 @@ async function rollSlugs(e) {
 
         resp = fetch('api/v1/slug?' + new URLSearchParams(params));
         await rebuildSlugs();
+
+        return;
     }
 
     visitSlugs((s) => {
